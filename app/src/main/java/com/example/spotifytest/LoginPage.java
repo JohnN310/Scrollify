de471e118aa9536b4ba6de5b85fc635a2ba0a26d
eshasingh05 <esingh37@gatech.edu>
2024-03-30 20:47:28 -0400

    loginpage java class

---
+++ app/src/main/java/com/example/spotifytest/LoginPage.java
@@ -0,0 +1,44 @@
+package com.example.spotifytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity  {
+
+    EditText inputUsername;
+    EditText inputPassword;
+
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputUsername = (EditText) findViewById(R.id.login_username);
+        inputPassword = (EditText) findViewById(R.id.login_password);
+
+
+        Button loginAttempt = (Button) findViewById(R.id.login_button);
+        loginAttempt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AccountsDatabaseHandler accountsDatabaseHandler = new AccountsDatabaseHandler(LoginPage.this);
+                ArrayList<YourProfile> accounts = accountsDatabaseHandler.readProfiles();
+
+
+            }
+        });
+
+
+
+    }
+
+
+
+
+}
